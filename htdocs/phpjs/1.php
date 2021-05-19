78c199edd7c449a616b7a158193a3b81fdbf12ed
SangYoonLee <prestro1231@hufs.ac.kr>
2021-05-20 03:04:08 +0900

    Add files via upload

---
+++ htdocs/phpjs/1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+</head>
+<body>
+  <?php
+		echo "Hello World ";
+    echo 10+10;
+  ?>
+	<h1>JavaScript</h1>
+	<script>
+		document.write("Hello World ");
+		document.write(10+10);
+	</script>
+</body>
+</html>
